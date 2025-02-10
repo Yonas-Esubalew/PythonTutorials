35f23d5f7ae24e397eec3f8b5775a8a49ec4aa8f
yonas pro <yonasbe999@gmail.com>
2025-02-10 02:36:49 -0800

    Second Class Code

--- pythonIF_ELSE.py
+++ pythonIF_ELSE.py
@@ -83,7 +83,7 @@ elif unit == "L":
 else:
     print(F"{unit} was not Valid.")      
     
-#  Temprature Convertor CodeC
+#  Temprature Convertor Code
 unit = input("is this Tem is Celcius or Farenheit (C/F): ")
 temp = float(input("Enter The Temprature: "))
 
@@ -98,3 +98,127 @@ elif unit == "F":
 else:
     print(F"{unit} Is an Invalid Unit of Measurenment")    
         
+        # logical operators = evaluate Multiple conditinos (or, and, not)
+        
+        # or = at least one conditinos must be true
+        # and = both conditions are must be true
+        # not = inverts the condition(not false, not true)
+temp = 25 
+is_raining = True
+
+if temp > 35 or temp < 0 or is_raining:
+    print("the schedule are Cancelled ")    
+else:
+    print("the schedule are still Sheduled")    
+temp  = -7
+is_sunny = True
+
+if temp >= 28 and is_sunny:
+    print("it is hot outside 🎆")
+    print("its sunny 🎉")
+elif temp <= 0 and  not is_sunny:
+    print("it is Cold outside")   
+    print("it is sunny") 
+elif 28 > temp > 0 and not is_sunny:
+    print("it is WARM outside 🧥")
+    print("it is SUNNY 🎉")
+else:
+    print("normal value of the formal stuation")
+          
+          
+    #  conditional expression = a one-line shortcut of if-else statement (ternary operator )
+    #  print or Assign one of two values based on a condition
+    # X if Condition else Y
+    
+num = 6
+a = 4 
+b = 5
+
+
+print("positive" if num > 0 else "Negative")
+
+result = "EVEN" if num % 2 == 0 else "ODD"
+print(result)
+
+max_num = a if a > b else b
+print(max_num)
+
+min_num = b if b > a else a
+print(min_num)
+
+age = 3
+status = "adult" if age>= 18 else "Child"
+print(status)
+
+temp = 0
+weather  = "HOT" if temp > 20 else "COLD"
+print(weather)
+      
+user_role = ""
+
+access_level = "Full acess" if user_role == "Admin" else "Limited Acess"    
+print(access_level)
+
+# username must not contain digits
+
+username = input("Enter A username: ")
+
+if len(username ) > 12:
+    print("your username more than 12 characters")
+elif not username.find(" ") == -1:
+    print("your username can't contain spaces")   
+elif not username.isalpha():
+    print("your username can't contain numbers")
+else:
+    print(F"welcome to {username}")
+    
+    
+    
+name = input("Enter your full name: ")
+result = len(name)
+result = name.find("i")
+print(result)
+
+update_name = name.lower()
+print(update_name)
+result = name.isdigit()
+print(result)
+result = name.isalpha()
+print(result)
+
+ex = input("enter the noun: ")
+
+result = ex.count('o')
+print(result)
+
+
+phone_number = input("please ener the number: ")
+number = phone_number.replace("-", "")
+print(number)
+
+
+# validate user input exercise
+# username is no more than 12 characters
+# username must not contain spaces
+# username must not contain digits
+
+
+username = input("Please Enter the Username: ")
+
+length = len(username)
+
+if length > 12:
+    print("username is no more than 12")
+elif not username.find(" ")   == -1:
+    print("username must not contain spaces")  
+elif not username.isalpha():
+    print("username must Not Contain Digits")    
+else:
+    print(F"welcome {username}")    
+
+
+ Inedexing Accessing elements of Sequence using [], [start, end, step]
+
+credit_number = "1244-34533-54454-34242"
+last_digits = credit_number[-4:]
+print(F"XXXX-XXXX-XXXX-{last_digits}")
