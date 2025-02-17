02f4f89944b326bb7f874c13028da2f01baaa199
yonas pro <yonasbe999@gmail.com>
2025-02-17 10:22:55 -0900

    commit one day

--- Python6.py
+++ Python6.py
@@ -97,44 +97,102 @@
 # python banking program
 
 
-balance = 0
-# balanc = balance + 2
-is_running = True
+# balance = 0
+# # balanc = balance + 2
+# is_running = True
 
-def show_balance():
-    print(F"your balance is ${balance:.2f}")
-def deposit():
-    amount = input("Enter an amount to be deposited: ")
-    if amount < 0:
-        print("that is not a valid amount")
-    else:
-        return amount
-def withdraw():
+# def show_balance():
+#     print(F"your balance is ${balance:.2f}")
+# def deposit():
+#     amount = input("Enter an amount to be deposited: ")
+#     if amount < 0:
+#         print("that is not a valid amount")
+#     else:
+#         return amount
+# def withdraw():
+#     amount  = float(input("Enter amount to be withdrawn: "))
+    
+#     if amount > balance:
+#         print("Insufficeint Fields")
+#         return 0 
+#     elif amount < 0:
+#         print("Amount must be Greater than 0")
+#     else:
+#         return  amount
+
+
+# balance = 0
+# # balanc = balance + 2
+# is_running = True
+
+# while is_running:
+#     print("Banking Program")
+#     print("1.Show Balance")
+#     print("2.Deposit")
+#     print("3.Withdraw")
+#     print("4.Exit")
+    
+#     choice  = input("Enter your Chioce (1/4): ")
+    
+#     if choice == "1":
+#         show_balance()
+#     elif choice  == "2":
+#         balance += deposit()
+#     elif choice == "3":
+#         balance -= withdraw()
+        
+#     elif choice == "4":
+#         is_running = False
+#     else:
+#         print("That is not valid Number")     
+# print("thank you! have a nice day")    
+
+        #   python slot machine
+# def spin_row():
+#     pass
+# def print_row():
+#     pass
+# def get_payout():
+#     pass
+# def main():
+#     balance = 100
+    
+#     print("*******************")
+#     print("welcome to Python SLots!")
+#     print("Symbol: 😝💕😍🤣")
+#     while balance > 0:
+#         print(F"current balance: {balance}")
+#         bet  = input("place your bet amount: ")
+# if __name__ == "__main__":
+#     main()
+
+# pyhton slot Machine
+
+def spin_row():
     pass
 
+def print_row():
+    pass
 
-balance = 0
-# balanc = balance + 2
-is_running = True
+def get_payout():
+    pass
 
-while is_running:
-    print("Banking Program")
-    print("1.Show Balance")
-    print("2.Deposit")
-    print("3.Withdraw")
-    print("4.Exit")
+def main():
+    balance =  100 
+    print("*******************")
+    print("WElcome python Slots")
+    print("Symbols: 🤚 🎉 🍎 💎 ❤")
+    print("********************")
     
-    choice  = input("Enter your Chioce (1/4): ")
-    
-    if choice == "1":
-        show_balance()
-    elif choice  == "2":
-        balance += deposit()
-    elif choice == "3":
-        withdraw()
-    elif choice == "4":
-        is_running = False
-    else:
-        print("That is not valid Number")     
-print("thank you! have a nice day")              
-    
+    while balance > 0:
+        print(F"Current balance: {balance}")  
+        
+        bet = input("Please your bet Amount:  ")  
+        
+        if bet.isdigit():
+            print("Please enter a valid number: ")
+            continue
+        bet  = int(bet)
+        
+if __name__ == "__main__":
+    main()        
